8671b6676548bad2a3d9fa594345bbea99b63094
Donapieppo <donapieppo@yahoo.it>
2020-03-20 15:10:57 +0100

    authorize

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   skip_before_action :force_sso_user, :set_organization, :set_round
 
   def start
+    authorize :home
     if user_signed_in?
       if current_user.student?
         redirect_to punches_path and return
@@ -11,7 +12,6 @@ class HomeController < ApplicationController
         redirect_to profiles_path and return
       end
     end
-    authorize :home
   end
 end
 
