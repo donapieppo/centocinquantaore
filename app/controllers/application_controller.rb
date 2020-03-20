f781d1340b6ed33a78bbdfa9eeaeaba88ced9d44
Donapieppo <donapieppo@yahoo.it>
2020-03-20 15:10:47 +0100

    moved in dm_unibo_commmon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,22 +8,8 @@ class ApplicationController < ActionController::Base
 
   impersonates :user
 
-  before_action :log_current_user, :force_sso_user, :set_organization, :retrive_authlevels, :set_round
+  before_action :log_current_user, :force_sso_user, :set_organization, :update_current_user_authlevels, :set_round
   after_action :verify_authorized, except: [:index, :who_impersonate, :impersonate, :shibboleth]
-
-  # We set organization with params[:__org__] as organization_id in config/routes
-  def set_organization
-    if params[:__org__]
-      session[:oid] = params[:__org__].to_i 
-    end
-    # fallback to default organization
-    session[:oid] ||= 1
-
-    @current_organization = Organization.find(session[:oid].to_i)
-    if current_user
-      current_user.current_organization = @current_organization
-    end
-  end
 
   # iniziamo con Round.get_active
   # quando cambia lo mettiamo in sessione
