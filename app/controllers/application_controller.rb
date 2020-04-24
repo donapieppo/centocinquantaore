337cede5e0fce4aef5eaa23e6b463ed56d32a204
Donapieppo <donapieppo@yahoo.it>
2020-04-24 16:37:55 +0200

    refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,32 +18,16 @@ class ApplicationController < ActionController::Base
   # iniziamo con Round.get_active
   # quando cambia lo mettiamo in sessione
   def set_round
-    return unless current_user
-    if params[:round_id] 
-      @current_round = Round.find(params[:round_id])
-      session[:round_id] = @current_round.id
-    elsif session[:round_id]
-      @current_round = Round.find(session[:round_id])
-    else
-      @current_round = Round.get_default
-      session[:round_id] = @current_round.id
+    if current_user
+      if params[:round_id] 
+        @current_round = Round.find(params[:round_id])
+        session[:round_id] = @current_round.id
+      elsif session[:round_id]
+        @current_round = Round.find(session[:round_id])
+      else
+        @current_round = Round.get_default
+        session[:round_id] = @current_round.id
+      end
     end
   end
-
-  def user_secretary?
-    current_user.secretary?(@current_organization)
-  end
-
-  def user_student?
-    current_user.student?
-  end
-
-  def user_not_student!
-    current_user.student? and raise DmUniboCommon::NoAccess
-  end
-
-  def user_student!
-    current_user.student? or raise DmUniboCommon::NoAccess
-  end
-
 end
