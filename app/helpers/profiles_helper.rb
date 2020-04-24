23af7d9ae98937c2ed5719e1fa880f7e3ccb7dd4
Donapieppo <donapieppo@yahoo.it>
2020-04-24 16:41:15 +0200

    dmicon

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,21 +1,21 @@
 module ProfilesHelper
 
   def link_to_close_profile(profile)
-    link_to icon('birthday-cake') + ' Lo studente ha <strong>terminato</strong> il servizio'.html_safe, 
+    link_to dmicon('birthday-cake') + ' Lo studente ha <strong>terminato</strong> il servizio'.html_safe, 
             close_profile_path(profile), 
             method: :put, class: :button, 
             data: { confirm: 'Dopo la chiusura lo studente non può più inserire timbrature. Proseguire?' } 
   end
 
   def link_to_resign_profile(profile)
-    link_to icon('close') + ' Lo studente ha <strong>rinunciato</strong>'.html_safe, 
+    link_to dmicon('close') + ' Lo studente ha <strong>rinunciato</strong>'.html_safe, 
             resign_profile_path(profile), 
             method: :put, class: :button, 
             data: { confirm: 'Dopo la chiusura lo studente non può più inserire timbrature. Proseguire?' } 
   end
 
   def link_to_new_punch(profile)
-    link_to icon('credit-card') + ' Inserisci timbratura', new_profile_punch_path(profile), class: :button
+    link_to dmicon('credit-card') + ' Inserisci timbratura', new_profile_punch_path(profile), class: :button
   end
 end
 
