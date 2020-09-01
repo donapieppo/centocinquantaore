a33aa3f01a307dc48b9013506669429164e308d9
Donapieppo <donapieppo@yahoo.it>
2020-09-01 16:57:40 +0200

    back with before actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < DmUniboCommon::ApplicationController
-  before_action :set_round, :force_sso_user
+  before_action :set_current_user, :update_authorization, :set_current_organization, :log_current_user, :set_round, :force_sso_user
   after_action :verify_authorized, except: [:index, :who_impersonate, :impersonate, :shibboleth]
 
   # iniziamo con Round.get_active
