1f12c352d68d9686d2feb425d0e9c927ec8bf44a
Donapieppo <donapieppo@yahoo.it>
2020-10-02 12:58:47 +0200

    secretaries method

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -5,5 +5,9 @@ class Organization < ApplicationRecord
   has_many :profiles
 
   default_scope { order('organizations.name') }
+
+  def secretaries
+    self.users_with_permission_level(2)
+  end
 end
 
