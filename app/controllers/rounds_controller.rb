22d7745ba256fd2f628382fb5fa67b2a5e60c3ee
Donapieppo <donapieppo@yahoo.it>
2020-10-06 13:18:51 +0200

    auth

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,34 +1,40 @@
 class RoundsController < ApplicationController
-  before_action :user_admin!
+  before_action :get_round_and_check_permission, only: [:edit, :update]  
 
   def index
     @rounds = Round.all
+    authorize :round
   end
 
   # FIMXE NON RISCRIVERE @ROUND
   def new
     @round = Round.new
-  end
-
-  def edit
-    @round = Round.find(params[:id])
+    authorize @round
   end
 
   def create
     @round = Round.new(round_params)
+    authorize @round
     if @round.save
       redirect_to rounds_path
     end
   end
 
+  def edit
+  end
+
   def update
-    @round = Round.find(params[:id])
     if @round.update_attributes(round_params)
       redirect_to rounds_path
     end
   end
 
   private
+
+  def get_round_and_check_permission
+    @round = Round.find(params[:id])
+    authorize(@round)
+  end
 
   def round_params
     params.require(:round).permit(:start_date, :end_date)
