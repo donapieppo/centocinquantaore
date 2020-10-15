332dd822f873d7c9f78873b5fa58a231a20a6d0a
Donapieppo <donapieppo@yahoo.it>
2020-10-15 10:58:19 +0200

    config.authlevels

--- app/policies/organization_policy.rb
+++ app/policies/organization_policy.rb
@@ -1,4 +1,5 @@
 class OrganizationPolicy < DmUniboCommon::OrganizationPolicy
+  configure_authlevels
 
   def list_profiles?
     @user && @user.authorization && (@user.authorization.can_read?(@record) || (@user.area_ids & @record.area_ids).any?)
