1fc2a30bc05411c1c624166af94e2c1a6885192d
Pietro <donapieppo@yahoo.it>
2022-07-04 11:58:14 +0200

    btn

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,17 +1,16 @@
 module ProfilesHelper
-
   def link_to_close_profile(profile)
     button_to dmicon('birthday-cake') + ' Lo studente ha <strong>terminato</strong> il servizio'.html_safe, 
               close_profile_path(profile), 
               form: { data: { 'turbo-confirm': 'Dopo la chiusura lo studente non può più inserire timbrature. Proseguire?' },
-                      class: 'btn btn-primary' }
+                      class: 'btn btn-warning' }
   end
 
   def link_to_resign_profile(profile)
     button_to dmicon('close') + ' Lo studente ha <strong>rinunciato</strong>'.html_safe, 
               resign_profile_path(profile), 
               form: { data: { 'turbo-confirm': 'Dopo la chiusura lo studente non può più inserire timbrature. Proseguire?' },
-                      class: 'btn btn-primary' }
+                      class: 'btn btn-warning' }
   end
 
   def link_to_new_punch(profile)
